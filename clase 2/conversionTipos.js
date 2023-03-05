237071186ef72c40471df0a062ecbef5805420c2
Gabriel Martinez <martinegabriel47@gmail.com>
2023-03-05 12:57:41 -0500

    Create conversionTipos.js

---
+++ clase 2/conversionTipos.js
@@ -0,0 +1,19 @@
+//TIPOS DE DATOS.
+
+//Alfa numerico
+
+var nombrePasajero = "Pedro";
+console.log(nombrePasajero);
+
+//Tipo numero
+
+var valorBoleto = 1500;
+console.log(valorBoleto);
+
+//Logicos o Booleanos (True, False)
+
+var boletoActivo = true;
+console.log(boletoActivo);
+
+var boletoActivo = false;
+console.log(boletoActivo);
