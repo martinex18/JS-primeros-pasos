e4edca8b93e7de9133b74ebc37fce5c3924a9a70
Gabriel Martinez <martinegabriel47@gmail.com>
2023-03-05 13:19:13 -0500

    Update operaciones.js

---
+++ clase 2/operaciones.js
@@ -0,0 +1,41 @@
+//OPERACIONES ARITMETICAS
+
+//Orden de precedencia
+//Parentesis ()
+//Multiplicación * y Division /
+//Suma + y Resta -
+
+
+//Suma
+
+var valorBoleto = 12;
+var impuestoAeropuesto = 23;
+
+var totalBoletos = valorBoleto + impuestoAeropuesto;
+console.log(totalBoletos);
+
+//Resta
+
+var totalBoletos = impuestoAeropuesto - valorBoleto ;
+console.log(totalBoletos);
+
+//Multiplicacion
+
+var totalBoletos = impuestoAeropuesto * valorBoleto ;
+console.log(totalBoletos);
+
+//Division
+
+var totalBoletos = impuestoAeropuesto / valorBoleto ;
+console.log(totalBoletos);
+
+//CONCATENACION DE TEXTO
+
+var namePasajero = "Leo";
+var lastnamePasajero = "Cruz";
+
+var nombreCompleto = namePasajero + lastnamePasajero;
+var pasaporte = "1000" + "200";
+var divi = 
+
+console.log(nombreCompleto);
