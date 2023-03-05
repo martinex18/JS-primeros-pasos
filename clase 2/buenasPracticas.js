919da5dd6d8d5d0a6c34457e969bc205120247c0
Gabriel Martinez <martinegabriel47@gmail.com>
2023-03-05 12:39:40 -0500

    Creado buenasPracticas.js

---
+++ clase 2/buenasPracticas.js
@@ -0,0 +1,11 @@
+var nombreEmpleado = "Juan";
+var NombreEmpleado = "Carlos";
+
+console.log(nombreEmpleado);
+console.log(NombreEmpleado);
+
+var edadEmpleado = 40;
+console.log(edadEmpleado);
+
+var edadEmpleado = "Cuarenta";
+console.log(edadEmpleado);
