ca32607152b2db13337c7751c86febb591df69f7
Gabriel Martinez <martinegabriel47@gmail.com>
2023-03-05 12:56:50 -0500

    Update variables.js

--- clase1/variables.js
+++ clase1/variables.js
@@ -1,6 +1,6 @@
-var nombreEmpleado = "José Perez"; /*varible tipo texto*/
-var direccion = "Av.105 - Calle 3"; /*variable tipo Alfa numerico*/
-var costoPasaje = 1500; /*variable tipo entero*/
-var tasaEmbarque = 50.5; /*variable tipo decimal*/
+var nombreEmpleado = "José Perez"; //varible tipo texto
+var direccion = "Av.105 - Calle 3"; //variable tipo Alfa numerico
+var costoPasaje = 1500; //variable tipo entero
+var tasaEmbarque = 50.5; //variable tipo decimal
 
 console.log(nombreEmpleado, direccion, costoPasaje, tasaEmbarque);
