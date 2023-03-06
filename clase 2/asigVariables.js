3214c2dae395c2d7f339b9f524664d6ff50466aa
Gabriel Martinez <martinegabriel47@gmail.com>
2023-03-05 21:16:52 -0500

    Create asigVariables.js

---
+++ clase 2/asigVariables.js
@@ -0,0 +1,11 @@
+//PALABRA CONST
+//Espacio de memoria que luego de asignar su valor no cambia en el tiempo
+
+const valorPasaje = 1650;
+console.log(valorPasaje);
+
+const nombrePasajero = "Leo";
+const apellidoPasajero = "Cruz";
+console.log(nombrePasajero);
+console.log(apellidoPasajero);
+
